168be5fd5f8d5f9c7ea2c5f1ccb1ea0c29610f7c
FlutLab.io <>
2021-12-08 07:53:51 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/ui_designapp/generatednotifkeranjangwidget/GeneratedNotifkeranjangWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedkeranjangsayawidget/GeneratedKeranjangsayaWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedkalkulasiuspwidget/GeneratedKalkulasiUSPWidget.dart';
+import 'package:flutterapp/ui_designapp/generatednotifikasispwidget/GeneratedNotifikasiSPWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedsimulasipinjamanuspwidget/GeneratedSimulasipinjamanUSPWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedsimulasipinjamandlwidget/GeneratedSimulasipinjamanDLWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedsimulasipinjamanmlgwidget/GeneratedSimulasipinjamanMLGWidget.dart';
+import 'package:flutterapp/ui_designapp/generateddetaildestinasiwidget/GeneratedDetailDestinasiWidget.dart';
+import 'package:flutterapp/ui_designapp/generatednotifikasicheckoutwidget/GeneratedNotifikasiCheckOutWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedgrafikwidget9/GeneratedGrafikWidget9.dart';
+import 'package:flutterapp/ui_designapp/generatedtabungansukarelawidget/GeneratedTabungansukarelaWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedprofilewidget/GeneratedProfileWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedregistrasiuserwidget/GeneratedRegistrasiUserWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedanekausahawidget13/GeneratedAnekaUsahaWidget13.dart';
+import 'package:flutterapp/ui_designapp/generatedhomepageswidget/GeneratedHomePagesWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedmenuwidget/GeneratedMenuWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedwhatsappimage20211201at20231widget/GeneratedWhatsAppImage20211201at20231Widget.dart';
+import 'package:flutterapp/ui_designapp/generatedscreenshot_20211202122047_mysiskopadis1widget/GeneratedScreenshot_20211202122047_MySiskopAdis1Widget.dart';
+import 'package:flutterapp/ui_designapp/generatedh1201widget/GeneratedH1201Widget.dart';
+import 'package:flutterapp/ui_designapp/generatedh1225widget/GeneratedH1225Widget.dart';
+import 'package:flutterapp/ui_designapp/generatedh1060widget/GeneratedH1060Widget.dart';
+import 'package:flutterapp/ui_designapp/generatedstylecoloringwidget/GeneratedStyleColoringWidget.dart';
+import 'package:flutterapp/ui_designapp/generatediconwidget/GeneratedIconWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedtypographywidget/GeneratedTypographyWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedbuttomwidget/GeneratedButtomWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedgambarwisatawidget/GeneratedGambarWisataWidget.dart';
+import 'package:flutterapp/ui_designapp/generatedscreenshot_20211207140058_mysiskopadis1widget/GeneratedScreenshot_20211207140058_MySiskopAdis1Widget.dart';
+
+void main() {
+  runApp(UI_DesignApp());
+}
+
+class UI_DesignApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedNotifkeranjangWidget',
+      routes: {
+        '/GeneratedNotifkeranjangWidget': (context) =>
+            GeneratedNotifkeranjangWidget(),
+        '/GeneratedKeranjangsayaWidget': (context) =>
+            GeneratedKeranjangsayaWidget(),
+        '/GeneratedKalkulasiUSPWidget': (context) =>
+            GeneratedKalkulasiUSPWidget(),
+        '/GeneratedNotifikasiSPWidget': (context) =>
+            GeneratedNotifikasiSPWidget(),
+        '/GeneratedSimulasipinjamanUSPWidget': (context) =>
+            GeneratedSimulasipinjamanUSPWidget(),
+        '/GeneratedSimulasipinjamanDLWidget': (context) =>
+            GeneratedSimulasipinjamanDLWidget(),
+        '/GeneratedSimulasipinjamanMLGWidget': (context) =>
+            GeneratedSimulasipinjamanMLGWidget(),
+        '/GeneratedDetailDestinasiWidget': (context) =>
+            GeneratedDetailDestinasiWidget(),
+        '/GeneratedNotifikasiCheckOutWidget': (context) =>
+            GeneratedNotifikasiCheckOutWidget(),
+        '/GeneratedGrafikWidget9': (context) => GeneratedGrafikWidget9(),
+        '/GeneratedTabungansukarelaWidget': (context) =>
+            GeneratedTabungansukarelaWidget(),
+        '/GeneratedProfileWidget': (context) => GeneratedProfileWidget(),
+        '/GeneratedRegistrasiUserWidget': (context) =>
+            GeneratedRegistrasiUserWidget(),
+        '/GeneratedAnekaUsahaWidget13': (context) =>
+            GeneratedAnekaUsahaWidget13(),
+        '/GeneratedHomePagesWidget': (context) => GeneratedHomePagesWidget(),
+        '/GeneratedMenuWidget': (context) => GeneratedMenuWidget(),
+        '/GeneratedWhatsAppImage20211201at20231Widget': (context) =>
+            GeneratedWhatsAppImage20211201at20231Widget(),
+        '/GeneratedScreenshot_20211202122047_MySiskopAdis1Widget': (context) =>
+            GeneratedScreenshot_20211202122047_MySiskopAdis1Widget(),
+        '/GeneratedH1201Widget': (context) => GeneratedH1201Widget(),
+        '/GeneratedH1225Widget': (context) => GeneratedH1225Widget(),
+        '/GeneratedH1060Widget': (context) => GeneratedH1060Widget(),
+        '/GeneratedStyleColoringWidget': (context) =>
+            GeneratedStyleColoringWidget(),
+        '/GeneratedIconWidget': (context) => GeneratedIconWidget(),
+        '/GeneratedTypographyWidget': (context) => GeneratedTypographyWidget(),
+        '/GeneratedButtomWidget': (context) => GeneratedButtomWidget(),
+        '/GeneratedGambarWisataWidget': (context) =>
+            GeneratedGambarWisataWidget(),
+        '/GeneratedScreenshot_20211207140058_MySiskopAdis1Widget': (context) =>
+            GeneratedScreenshot_20211207140058_MySiskopAdis1Widget(),
+      },
+    );
+  }
+}
